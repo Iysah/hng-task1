c145220d561b160696c486250a3f14aca7867c96
Iysah <yusufiysah@gmail.com>
2022-11-02 21:44:48 +0100

    updated the links

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,17 @@ function App() {
   return (
     <div className="App">
       <div className='header'>
-        <img src={iysah} alt="Iysah Yusuf" />
-        <h1>Iysah Yusuf</h1>
+        <img src={iysah} id="profile__img" alt="Iysah Yusuf" />
+        <h1 id='twitter'>Iysah Yusuf</h1>
       </div>
 
       <div className="card">
-        <a href="https://training.zuri.team/">Twitter Link</a>
-        <a href="http://books.zuri.team/">Zuri Team</a>
-        <a href="https://books.zuri.team/">Zuri Books</a>
-        <a href="https://books.zuri.team/python-for-beginners?ref_id=yusufiysah">Python Books</a>
-        <a href="https://background.zuri.team/">Background Check for Coders</a>
-        <a href="https://books.zuri.team/">Design Checks</a>
+        <a id='twitter' href="https://training.zuri.team/">Twitter Link</a>
+        <a id='btn__zuri' href="http://books.zuri.team/">Zuri Team</a>
+        <a id='books' href="https://books.zuri.team/">Zuri Books</a>
+        <a id='book__python' href="https://books.zuri.team/python-for-beginners?ref_id=iysahyusuf">Python Books</a>
+        <a id='pitch' href="https://background.zuri.team/">Background Check for Coders</a>
+        <a id='book__design' href="https://books.zuri.team/design-rules">Design Checks</a>
 
         <div className="socials">
           <a href="#" id='slack'>
